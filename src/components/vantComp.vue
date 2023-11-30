e86b4ae47d239d63a203a06e11c9aced5a33d729
danut <matei1337@icloud.com>
2023-11-30 12:14:05 +0200

    component done

---
+++ src/components/vantComp.vue
@@ -0,0 +1,47 @@
+<template>
+    <van-tabs v-model:active="active" scrollspy sticky>
+        <van-tab v-for="index in 20" :title="'tab' + index" :key="index">
+            Items
+            <div class="products">
+                <van-row style="gap: 5px !important;">
+                    <van-col v-for="item in 10" :key="item">
+                        <van-card num="3" price="2.00" desc="Description" title="Title"
+                            thumb="https://img.yzcdn.cn/vant/ipad.jpeg" />
+                    </van-col>
+                </van-row>
+            </div>
+        </van-tab>
+    </van-tabs>
+</template>
+<script lang="ts">
+import { ref } from 'vue';
+
+export default {
+    setup() {
+        const active = ref(0);
+        const beforeChange = (index: number) => {
+            // prevent change
+            if (index === 1) {
+                return false;
+            }
+
+            // async
+            return new Promise((resolve) => {
+                resolve(index !== 3);
+            });
+        };
+
+        return {
+            active,
+            beforeChange,
+        };
+    },
+};
+</script>
+
+<style scoped>
+.products {
+    padding: 10px;
+    background-color: #fff;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import './assets/main.css'
+import 'vant/lib/index.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import { Tab, Tabs, Cell, CellGroup, Row, Col, Card } from 'vant'
 
-createApp(App).mount('#app')
+createApp(App).use(Tab).use(Tabs).use(Cell).use(CellGroup).use(Row).use(Col).use(Card).mount('#app')
